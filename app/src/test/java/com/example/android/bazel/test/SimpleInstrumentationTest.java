24b8c86708f330971c62e5b99d51f4d75c6270f1
Kirill Timofeev <ktimofeev@google.com>
2024-12-13 20:21:45 +0000

    Try to use 'android_instrumentation_test' Blaze rule.

---
+++ app/src/test/java/com/example/android/bazel/test/SimpleInstrumentationTest.java
@@ -0,0 +1,10 @@
+package com.example.android.bazel.test;
+
+import androidx.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(AndroidJUnit4.class)
+public class SimpleInstrumentationTest {
+    @Test
+    public void fooTest() {}
+}
